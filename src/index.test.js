36a81e107701a1175b72fdd9502eb5a88a128aa8
VaishK <shalu_822k@yahoo.co.in>
2018-04-26 18:14:33 +0530

    Demo to use Fetch

--- src/index.test.js
+++ src/index.test.js
@@ -12,7 +12,7 @@ describe("index.html", () => {
   it("should say Hello World", (done) => {
     JSDOM.fromFile("./src/index.html").then(dom => {
       const h1 = dom.window.document.body.getElementsByTagName("h1")[0];
-      expect(h1.innerHTML).to.equal("Hello World!");
+      expect(h1.innerHTML).to.equal("Users");
       done();
       dom.window.close();
     } ).catch(done);
